0f82c8deef50cbdc93cac5ae8da41d447a381b05
arpit <arpittyagi229@gmail.com>
2024-12-21 21:10:06 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// for toggling the menu
+
+var navLinks=document.getElementById("navLinks");
+function hideMenu(){
+    navLinks.style.right="-200px";
+    
+}
+function showMenu(){
+    navLinks.style.right="0px";
+}
